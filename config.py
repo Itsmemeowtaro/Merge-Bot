9517ae2b8dfb0d1d37aa0c6d28b186429a92dee8
Itsmemeowtaro <crowningshadow057@gmail.com>
2024-11-08 20:27:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,14 +2,14 @@ import os
 
 
 class Config(object):
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    API_HASH = os.environ.get("ef85493cd32eadcb5309b5957d8d1b86")
+    BOT_TOKEN = os.environ.get("7961888195:AAFk7fFOKDtybDb3FFJuZbLxttlAM1Wt6eY")
     TELEGRAM_API = os.environ.get("TELEGRAM_API")
-    OWNER = os.environ.get("OWNER")
-    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "CodeXBro")
-    PASSWORD = os.environ.get("PASSWORD")
-    DATABASE_URL = os.environ.get("DATABASE_URL")
-    LOGCHANNEL = os.environ.get("LOGCHANNEL")  # Add channel id as -100 + Actual ID
+    OWNER = os.environ.get("6440021089")
+    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "@Itsme_meowtaro")
+    PASSWORD = os.environ.get("meowtaro")
+    DATABASE_URL = os.environ.get("mongodb+srv://meow:meow@meow.a6bo1.mongodb.net/?retryWrites=true&w=majority&appName=meow")
+    LOGCHANNEL = os.environ.get("-1002134572304")  # Add channel id as -100 + Actual ID
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "root")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", None)
     IS_PREMIUM = False
