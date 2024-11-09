5464e2172b1846a78f03aeb8241a6b911e99d883
Itsmemeowtaro <crowningshadow057@gmail.com>
2024-11-09 14:06:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 class Config(object):
     API_HASH = os.environ.get("ef85493cd32eadcb5309b5957d8d1b86")
     BOT_TOKEN = os.environ.get("7961888195:AAFk7fFOKDtybDb3FFJuZbLxttlAM1Wt6eY")
-    TELEGRAM_API = os.environ.get("TELEGRAM_API")
+    TELEGRAM_API = os.environ.get("22606849")
     OWNER = os.environ.get("6440021089")
     OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "@Itsme_meowtaro")
     PASSWORD = os.environ.get("meowtaro")
